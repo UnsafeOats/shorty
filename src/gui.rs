ecb9d7747462875df236fe5eb0dc9d0e13f54ab7
Shane Stephenson <stephenson.shane.a@gmail.com>
2023-04-03 22:15:50 -0400

    working with search bar

--- src/gui.rs
+++ src/gui.rs
@@ -1,6 +1,7 @@
 use crate::configs;
 use anyhow::Result;
-use fltk::{prelude::*, *};
+use fltk::{prelude::*, *, enums::*, window};
+use std::{rc::Rc, cell::RefCell};
 
 pub struct GuiApp {
     configs: configs::Configs,
@@ -34,18 +35,38 @@ impl GuiApp {
         let y_offset = y - self.configs.settings.height + 5;
         let menu_width = self.configs.settings.width - MENU_OFFSET;
         let menu_height = self.configs.settings.height - MENU_OFFSET;
-        let mut wind = window::Window::new(x_offset, y_offset, self.configs.settings.width + MENU_OFFSET, self.configs.settings.height + MENU_OFFSET, "shrtcut");
-        let mut menu = menu::Choice::new(MENU_OFFSET, MENU_OFFSET, menu_width, menu_height, None);
+        // Create a new window for the input field and drop-down menu
+        let mut input_wind = window::Window::new(x_offset, y_offset, self.configs.settings.width + MENU_OFFSET, 30, "shrtcut");
+
+        // Create the input field
+        let mut input_buffer = input::Input::new(MENU_OFFSET, MENU_OFFSET, menu_width, 25, None);
+        input_buffer.set_trigger(CallbackTrigger::Changed);
+
+        // Create the drop-down menu
+        let menu_rc = Rc::new(RefCell::new(menu::Choice::new(MENU_OFFSET, MENU_OFFSET + 25, menu_width, menu_height, None)));
         for choice in self.choices.clone().iter() {
-            menu.add_choice(choice);
+            menu_rc.borrow_mut().add_choice(choice);
         }
-        wind.end();
-        wind.show();
+
+        let menu_rc_clone = menu_rc.clone();
+        let choices_clone = self.choices.clone();
+        input_buffer.set_callback(move |input_buffer| {
+            let input_text = input_buffer.value();
+            let filtered_choices = choices_clone.iter().filter(|c| c.starts_with(&input_text)).map(|c| c.to_string()).collect::<Vec<String>>();
+
+            menu_rc_clone.borrow_mut().clear();
+            for choice in filtered_choices {
+                menu_rc_clone.borrow_mut().add_choice(&choice);
+            }
+        });
+
+        input_wind.end();
+        input_wind.show();
         while app::wait() {
-            if menu.value() >= 0 {
-                self.configs.use_shortcut(self.choices[menu.value() as usize].clone())?;
-                menu.hide();
-                wind.hide();
+            if menu_rc.borrow().value() >= 0 {
+                self.configs.use_shortcut(self.choices[menu_rc.borrow().value() as usize].clone())?;
+                menu_rc.borrow_mut().hide();
+                input_wind.hide();
                 break;
             }
         }
